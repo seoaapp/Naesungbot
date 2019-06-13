72f244e724459f1a39c4b277c02f4d22fce8c358
jiho lee <jiholee@kpu.ac.kr>
2019-06-13 20:12:19 +0900

    Create locale.js

---
+++ locale.js
@@ -0,0 +1,47 @@
+const filehandler = require('./filehandler');
+
+let list;
+const refresh = () => {
+    try {
+        delete require.cache[require.resolve('./data/locale.json')];
+    } catch (e) {
+
+    }
+    try {
+        list = require('./data/locale.json');
+    } catch (e) {
+        list = [];
+    }
+    return list;
+};
+
+exports.get = () => refresh();
+
+
+const save = () => {
+    filehandler.saveFile('locale.json', JSON.stringify(list));
+};
+
+exports.add = (id) => {
+    refresh();
+    list.push(Number(id));
+    console.log(list);
+    save();
+};
+
+exports.remove = (id) => {
+    refresh();
+    id = Number(id);
+    if (list.includes(id)) {
+        list = list.filter(guessNotBlackList => guessNotBlackList !== id);
+        save();
+        return true;
+    } else {
+        return false;
+    }
+};
+
+exports.check = function (id) {
+    refresh();
+    list.includes(Number(id));
+};
