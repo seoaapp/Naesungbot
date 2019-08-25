dc11d68e1710f66d52f755a6ff3d52529da3c4fe
Naesung <42634731+Naesung@users.noreply.github.com>
2019-08-25 13:24:03 +0900

    Update premium.js

--- premium.js
+++ premium.js
@@ -1,1 +1,23 @@
+const filehandler = require('./filehandler');
 
+try {
+    delete require.cache[require.resolve('./data/premium.json')];
+} catch (e) {
+
+}
+try {
+    exports.list = require('./data/premium.json');
+} catch (e) {
+    exports.list = [];
+}
+
+
+const save = function () {
+    filehandler.saveFile('premium.json', JSON.stringify(exports.list));
+};
+
+exports.check = function (id) {
+    return exports.list.includes(Number(id));
+};
+
+// 관리 TODO
