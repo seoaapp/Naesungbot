59b75e0588ded844bf59936f9c0ce206ba8e0316
Naesung <42634731+Naesung@users.noreply.github.com>
2019-09-01 16:50:57 +0900

    Update subway.js

--- modules/subway.js
+++ modules/subway.js
@@ -6,4 +6,6 @@ const client = require('../client').client;
 const stringhandler = require('../stringhandler');
 const request = require('request');
 
+//beta 서비스입니다. 일단 규모가 적당한 부산광역시 대상.
 module.exports = {
+  'busan': (msg, command) => {
